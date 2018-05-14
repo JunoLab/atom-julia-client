69b666c18a476d45f2d849d773e3cebf2d503e36
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-05-14 19:42:23 +0200

    get rid of unnecessary code

--- script/boot_repl.jl
+++ script/boot_repl.jl
@@ -1,35 +1,16 @@
 let
-
-install = Base.find_in_path("Atom") == nothing
-
-if install
-  print(STDERR, "juno-msg-installing")
-  try
-    Pkg.add("Atom")
-  catch
-    print(STDERR, "juno-msg-install")
-    rethrow()
-  end
-end
-
 port = parse(Int, shift!(ARGS))
-
-precompile = !install && isempty(Base.find_all_in_cache_path(:Atom))
-
-if precompile
-  print(STDERR, "juno-msg-precompiling")
-end
 
 junorc = abspath(homedir(), ".junorc.jl")
 
 try
   import Atom
   using Juno
-  Atom.connect(port, welcome = precompile || install)
   Atom.handle("connected") do
     ispath(junorc) && include(junorc)
     nothing
   end
+  Atom.connect(port)
 catch
   print(STDERR, "juno-msg-load")
   rethrow()
