a67570cc542edae6d875b04514b8face98c1fa76
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-08-10 13:40:07 +0100

    fix legacy console

--- script/boot_sync.jl
+++ script/boot_sync.jl
@@ -1,39 +1,38 @@
 let
-
-install = Base.find_in_path("Atom") == nothing
-
-if install
-  print(STDERR, "juno-msg-installing")
-  try
-    Pkg.add("Atom")
-  catch
-    print(STDERR, "juno-msg-install")
-    rethrow()
-  end
-end
-
-port = parse(Int, shift!(ARGS))
-
-precompile = !install && isempty(Base.find_all_in_cache_path(:Atom))
-
-if precompile
-  print(STDERR, "juno-msg-precompiling")
+if VERSION > v"0.7-"
+  port = parse(Int, popfirst!(ARGS))
+else
+  port = parse(Int, shift!(ARGS))
 end
 
 junorc = abspath(homedir(), ".junorc.jl")
+
+if (VERSION > v"0.7-" ? Base.find_package("Atom") : Base.find_in_path("Atom")) == nothing
+  p = VERSION > v"0.7-" ? (x) -> printstyled(x, color=:cyan, bold=true) : (x) -> print_with_color(:cyan, x, bold=true)
+  p("\nHold on tight while we're installing some packages for you.\nThis should only take a few seconds...\n\n")
+
+  if VERSION > v"0.7-"
+    using Pkg
+    Pkg.activate()
+  end
+
+  Pkg.add("Atom")
+  Pkg.add("Juno")
+
+  println()
+end
 
 try
   import Atom
   using Juno
   @sync begin
-    Atom.connect(port, welcome = precompile || install)
-    Atom.handle("connected") do
+    Atom.handle("junorc") do
       ispath(junorc) && include(junorc)
       nothing
     end
+    Atom.connect(port)
   end
 catch
-  print(STDERR, "juno-msg-load")
   rethrow()
 end
 
