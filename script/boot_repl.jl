1bf620655ba21b8c5ae82a1d9375c2ee2954a2ed
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-11-27 11:37:32 +0100

    better remote startup

--- script/boot_repl.jl
+++ script/boot_repl.jl
@@ -12,22 +12,7 @@ junorc = abspath(normpath(expanduser(junorc)))
 
 if (VERSION > v"0.7-" ? Base.find_package("Atom") : Base.find_in_path("Atom")) == nothing
   p = VERSION > v"0.7-" ? (x) -> printstyled(x, color=:cyan, bold=true) : (x) -> print_with_color(:cyan, x, bold=true)
-  p("\nHold on tight while we're installing some packages for you.\nThis should only take a few seconds...\n\n")
-
-  if VERSION > v"0.7-"
-    using Pkg
-    Pkg.activate()
-  end
-
-  Pkg.add("Atom")
-  Pkg.add("Juno")
-
-  println()
-end
-
-if (VERSION > v"0.7-" ? Base.find_package("Atom") : Base.find_in_path("Atom")) == nothing
-  p = VERSION > v"0.7-" ? (x) -> printstyled(x, color=:cyan, bold=true) : (x) -> print_with_color(:cyan, x, bold=true)
-  p("\nHold on tight while we're installing some packages for you.\nThis should only take a few seconds...\n\n")
+  p("\nHold on tight while we are installing some packages for you.\nThis should only take a few seconds...\n\n")
 
   if VERSION > v"0.7-"
     using Pkg
