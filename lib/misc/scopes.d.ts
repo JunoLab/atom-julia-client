cf9f1d9a9b4ec2cf87ed9c1bfa049b58a1a57f23
aminya <aminyahyaabadi74@gmail.com>
2020-03-29 01:29:26 -0500

    npm run build

---
+++ lib/misc/scopes.d.ts
@@ -0,0 +1,10 @@
+import { TextEditor, PointCompatible } from "atom";
+export declare function isStringScope(scopes: readonly string[]): boolean;
+export declare function forLines(editor: TextEditor, start: number, end: number): string[];
+export declare function isCommentScope(scopes: readonly string[]): boolean;
+/**
+ * Returns `true` if the scope at `bufferPosition` in `editor` is valid code scope to be inspected.
+ * Supposed to be used within Atom-IDE integrations, whose `grammarScopes` setting doesn't support
+ * embedded scopes by default.
+ */
+export declare function isValidScopeToInspect(editor: TextEditor, bufferPosition: PointCompatible): boolean;
