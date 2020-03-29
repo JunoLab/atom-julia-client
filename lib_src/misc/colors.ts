18d81a0f90273945e74c118ace17d58839ed49d7
aminya <aminyahyaabadi74@gmail.com>
2020-03-28 21:25:35 -0500

    Taking hex function out

--- lib_src/misc/colors.ts
+++ lib_src/misc/colors.ts
@@ -36,9 +36,10 @@ function rgb2hex(rgb) {
     return rgb
   } else {
     rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/)
-    function hex(x) {
-      return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
     return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
   }
+}
+
+function hex(x) {
+  return ("0" + parseInt(x).toString(16)).slice(-2);
 }
