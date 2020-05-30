3b546d48da11f4e86edae26cbc19bf6245b7d6f2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-05-30 03:47:33 -0500

    Katex code from ink
    Update postinstall.js

--- script/postinstall.js
+++ script/postinstall.js
@@ -1,8 +1,22 @@
 var fs = require('fs')
+var fsextra = require('fs-extra')
+var replace = require('replace-in-file')
 
 function copyKeymaps () {
   let suffix = process.platform === 'darwin' ? '.cmd' : '.ctrl'
   fs.copyFileSync(__dirname + '/../keymaps/julia-client.cson' + suffix, __dirname + '/../keymaps/julia-client.cson')
 }
 
+function installKaTeX () {
+  fsextra.copySync(__dirname + '/../node_modules/katex/dist/fonts', __dirname + '/../styles/fonts')
+  fsextra.copySync(__dirname + '/../node_modules/katex/dist/katex.css', __dirname + '/../styles/katex.css')
+
+  replace.sync({
+    files: __dirname + '/../styles/katex.css',
+    from:  /url\(fonts/g,
+    to:    'url(atom:\/\/ink\/styles\/fonts'
+  })
+}
+
 copyKeymaps()
+installKaTeX()
